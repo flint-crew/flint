adaa7f9bdc5740e475d65c9dbbc45c3d6489eac6
tgalvin <tim.galvin@csiro.au>
2025-03-21 16:29:55 +0800

    added stubs

---
+++ tests/test_addmodel.py
@@ -0,0 +1,23 @@
+"""Basic tests around addmodel"""
+
+from __future__ import annotations
+
+from flint.predict.addmodel import get_parser
+
+
+def test_get_parser():
+    """No silly business around creating the parser (including imports)"""
+
+    _ = get_parser()
+
+
+def test_create_add_model_instance():
+    """Creating the addmodeloptions instance"""
+
+    raise ValueError()
+
+
+def test_addmodel_to_command_string():
+    """make sure we generate the correct command string"""
+
+    raise ValueError()
