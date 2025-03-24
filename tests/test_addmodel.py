34dc1c8c155dba7b9a33bde019107d074a687168
tgalvin <tim.galvin@csiro.au>
2025-03-24 15:39:43 +0800

    moved old test to new area

--- tests/test_addmodel.py
+++ tests/test_addmodel.py
@@ -40,6 +40,24 @@ def test_create_add_model_instance():
         )
 
 
+def test_generate_add_model_command():
+    """Ensure we can actually generate the expected addmodel cli command"""
+
+    add_model_options = AddModelOptions(
+        model_path=Path("/jack/sparrow/be/here/SB-sources.txt"),
+        ms_path=Path("/jack/sparrow/be/here/SB.ms"),
+        mode="c",
+        datacolumn="MODEL_DATA",
+    )
+
+    add_model_command = add_model_options_to_command(
+        add_model_options=add_model_options
+    )
+
+    expected_command = "addmodel -datacolumn MODEL_DATA -m c /jack/sparrow/be/here/SB-sources.txt /jack/sparrow/be/here/SB.ms"
+    assert add_model_command == expected_command
+
+
 def test_addmodel_to_command_string():
     """make sure we generate the correct command string"""
 
