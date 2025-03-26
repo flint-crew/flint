dd7e5f3579b799a041898a9039c6d1628f2a151a
tjgalvin <tjgalvin@users.noreply.github.com>
2025-03-26 14:27:24 +0800

    Fixes docker devcontainer.json for failing tests (#277)
    * updated some logic / binding / installing rsync
    
    * patching an error on new sqlite by forcing good version
    
    * added a note to github issue
    
    * moved the rsync to dev container
    
    * addresses comments
    
    * no mounts
    
    ---------
    
    Co-authored-by: tgalvin <tim.galvin@csiro.au>
    Co-authored-by: Alec Thomson <AlecThomson@users.noreply.github.com>

--- tests/test_prefect_helpers.py
+++ tests/test_prefect_helpers.py
@@ -3,8 +3,16 @@
 from __future__ import annotations
 
 from prefect import flow
+from prefect.logging import disable_run_logger
+from prefect.testing.utilities import prefect_test_harness
 
 from flint.prefect.helpers import enable_loguru_support
+
+
+@flow
+def example_flow():
+    enable_loguru_support()
+    return "JackSparrow"
 
 
 def test_enable_loguru_support():
@@ -15,8 +23,5 @@ def test_enable_loguru_support():
     error, though whether it still works is a completely different
     question!"""
 
-    @flow
-    def example_flow():
-        enable_loguru_support()
-
-    example_flow()
+    with prefect_test_harness(), disable_run_logger():
+        assert example_flow() == "JackSparrow"
