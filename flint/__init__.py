2b4d79a48f9de10f82f36dd0994a42b2d149f98f
Alec Thomson <AlecThomson@users.noreply.github.com>
2025-03-27 14:54:42 +0800

    Fix init (#282)
    * Explicit init
    
    * Add test
    
    * Clean up init
    
    * Make installs editable
    
    * Docs

--- flint/__init__.py
+++ flint/__init__.py
@@ -3,4 +3,7 @@ from __future__ import annotations
 from ._version import version as __version__
 from ._version import version_tuple as __version_tuple__
 
-__all__ = ["__version__", "__version_tuple__"]
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+]
