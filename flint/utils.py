0e27fb45ec4217f1fe032706385f28b1fcbb0671
tgalvin <tim.galvin@csiro.au>
2025-08-09 10:38:34 +1000

    added a todo

--- flint/utils.py
+++ flint/utils.py
@@ -136,6 +136,7 @@ def hold_then_move_into(
 
         for file_or_folder in hold_directory.glob("*"):
             logger.info(f"Moving {file_or_folder=} to {move_directory=}")
+            # TODO: Make this optional
             out_files = move_directory / file_or_folder.name
             if out_files.exists():
                 remove_files_folders(out_files)
