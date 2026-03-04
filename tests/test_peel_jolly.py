17793f0160546a790cb8cce16469acec7f6ab446
Alec Thomson <alec.thomson@skao.int>
2026-03-04 11:28:45 +0800

    Fix tests

--- tests/test_peel_jolly.py
+++ tests/test_peel_jolly.py
@@ -4,13 +4,12 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import astropy.units as u
 import pytest
-from jolly_roger.tractor import TukeyTractorOptions as JollyTukeyTaperOptions
+from jolly_roger.tractor import TukeyTractorOptions
 
 from flint.options import MS
 from flint.peel.jolly import (
-    TukeyTractorOptions,
-    _create_jolly_tractor_options,
     jolly_roger_tractor,
 )
 
@@ -21,21 +20,10 @@ def test_raise_bad_tukey_options() -> None:
 
     ms_path = Path("JackBeHere.ms")
     with pytest.raises(TypeError):
-        _create_jolly_tractor_options(
+        TukeyTractorOptions(
             ms_path=ms_path,
-            tukey_tractor_options=1.0,  # type: ignore
+            elevation_cut_deg=1.0 * u.deg,  # type: ignore
         )
-
-
-def test_convert_tractor() -> None:
-    """Make sure that we are converting the flint tractor options class
-    to the jolly-roget tractor class. This is needed to avoid serialising
-    astropy units."""
-
-    ms_path = Path("JackSparrow.ms")
-
-    tukey_tractor_options = TukeyTractorOptions(ms_path=ms_path, target_objects=["sun"])
-    assert isinstance(tukey_tractor_options, JollyTukeyTaperOptions)
 
 
 def test_tukey_tractor(ms_example) -> None:
