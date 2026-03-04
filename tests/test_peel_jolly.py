c6812050ed86e2481926f074f36518fbc8834ee5
Alec Thomson <alec.thomson@skao.int>
2026-03-04 14:09:17 +0800

    Fix the tests

--- tests/test_peel_jolly.py
+++ tests/test_peel_jolly.py
@@ -19,10 +19,8 @@ def test_raise_bad_tukey_options() -> None:
     """Ensure that the type converter is raising an error
     on an incorrect type"""
 
-    ms_path = Path("JackBeHere.ms")
     with pytest.raises(ValidationError):
         TukeyTractorOptions(
-            ms_path=ms_path,
             elevation_cut_deg=1.0 * u.deg,  # type: ignore
         )
 
@@ -32,7 +30,7 @@ def test_tukey_tractor(ms_example) -> None:
 
     ms_path = Path(ms_example)
 
-    tukey_tractor_options = TukeyTractorOptions(ms_path=ms_path, target_objects=["sun"])
+    tukey_tractor_options = TukeyTractorOptions()
 
     ms = jolly_roger_tractor(ms=ms_path, tukey_tractor_options=tukey_tractor_options)
     assert isinstance(ms, MS)
