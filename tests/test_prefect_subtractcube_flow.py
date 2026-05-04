617f1a480b86c681235dcd1871a4e114c797b3a4
tgalvin <tim.galvin@csiro.au>
2026-05-04 10:07:36 +0800

    fixed typos raised in CI

--- tests/test_prefect_subtractcube_flow.py
+++ tests/test_prefect_subtractcube_flow.py
@@ -6,6 +6,6 @@ from flint.prefect.flows.subtract_cube_pipeline import get_parser
 
 
 def test_get_parser():
-    """Make sure the parser can actually be builts"""
+    """Make sure the parser can actually be built"""
     # This is a silly one but I was bitten by it not working, arrrrhh matey
     _ = get_parser()
