63a788fabc304d86b224c58f104e90274ca1bf7c
tgalvin <tim.galvin@csiro.au>
2026-05-05 14:42:18 +0800

    added block size

--- flint/misc/holo.py
+++ flint/misc/holo.py
@@ -300,6 +300,7 @@ def reproject_cubes(
                         spatial_header,
                         shape_out=shape_out_sky,
                         parallel=True,
+                        block_size=(10, 10),
                     )
                     out[beam, stokes, ch_out[ch_idx], :, :] = reprojected.astype(
                         np.float32
