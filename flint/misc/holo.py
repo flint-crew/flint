aef5f6d1a82a5293c129e570a9bc693d022e619e
tgalvin <tim.galvin@csiro.au>
2026-05-06 15:25:55 +0800

    r+ mode

--- flint/misc/holo.py
+++ flint/misc/holo.py
@@ -372,7 +372,7 @@ def create_placeholder_cube(
         frequency_grid=frequency_grid,
     )
     logger.info("Generating placeholder data")
-    with open(output_path, "wb") as f:
+    with open(output_path, "r+") as f:
         f.write(output_header.tostring())
         f.seek(np.prod(output_shape * 4), 1)
 
