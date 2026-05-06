7c79425f1c38c8086ff397ed6e423b877c0cc3fe
tgalvin <tim.galvin@csiro.au>
2026-05-06 15:40:38 +0800

    silly bytes

--- flint/misc/holo.py
+++ flint/misc/holo.py
@@ -373,7 +373,7 @@ def create_placeholder_cube(
     )
     logger.info("Generating placeholder data")
     with open(output_path, "rb") as f:
-        f.write(output_header.tostring().encode("ascii"))
+        f.write(output_header.tostring().encode())
         f.seek(np.prod(output_shape) * 4, 1)
 
     # arr = np.full(output_shape, np.nan, dtype=np.float32)
