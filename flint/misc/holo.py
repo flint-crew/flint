3a789f918cbe0217914dc5e2459dfc3af3aec33e
Tim Galvin <tim.galvin@csiro.au>
2026-08-18 13:25:49 +0800

    sanity checking

--- flint/misc/holo.py
+++ flint/misc/holo.py
@@ -23,9 +23,9 @@ from flint.logging import logger
 class ConcatHoloOptions(BaseOptions):
     """Options to use to concatenate holography cubes today"""
 
-    output_path: Path
+    output_path: Path | None = None
     """Output holography cube to make"""
-    holo_cubes: tuple[Path, ...]
+    holo_cubes: tuple[Path, ...] | None = None
     """The path to the holography IQUV cubes to concatenate together"""
     max_workers: int = 8
     """The maximum number of sub-processes that may be spawned"""
@@ -520,6 +520,14 @@ def create_output_header(
     return out_header
 
 
+def _check_concat_holo_options(concat_holo_options: ConcatHoloOptions) -> None:
+    """Check to ensure appropriate options have been set. Some of these are made up
+    programmatically in the process of a workflow, so a sanity check is made here."""
+
+    assert concat_holo_options.output_path is not None, "Unsure output path"
+    assert concat_holo_options.holo_cubes is not None, "Unset cube"
+
+
 def concatenate_holography(concat_holo_options: ConcatHoloOptions) -> Path:
     """Reproject a set of ASKAP IQUV primary beam cubes into a single output cube.
     An optimal spatial grid is computed internally through ``reproject``, and input
@@ -535,10 +543,14 @@ def concatenate_holography(concat_holo_options: ConcatHoloOptions) -> Path:
     logger.info("Attempting to concatenate holography cubes")
     logger.info(f"Running on host {gethostname()}")
 
+    _check_concat_holo_options(concat_holo_options=concat_holo_options)
+
+    assert concat_holo_options.holo_cubes is not None, "Holography cubes are unset."
     fits_cube_infos = load_and_sort_cubes(cube_paths=concat_holo_options.holo_cubes)
     spatial_header = construct_spatial_output_wcs(fits_cube_infos=fits_cube_infos)
     frequency_grid = construct_frequency_grid(fits_cube_infos=fits_cube_infos)
 
+    assert concat_holo_options.output_path is not None, "Unset output path"
     final_fits_cube_info: FinalFITSCubeInfo = create_placeholder_cube(
         fits_cube_infos=fits_cube_infos,
         spatial_header=spatial_header,
